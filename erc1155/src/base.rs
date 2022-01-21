39e542df3aabc8c21c93ef0d6200d5d83deab5a1
liushooter <shooter.hero@gmail.com>
2022-01-21 13:08:26 +0800

    Erc1155TokenBase

---
+++ erc1155/src/base.rs
@@ -0,0 +1,17 @@
+use gstd::{prelude::*, ActorId};
+use primitive_types::U256;
+
+pub trait Erc1155TokenBase {
+    fn init(&mut self, name: String, symbol: String, base_uri: String);
+
+    fn safe_transfer_from(&mut self, from: &ActorId, to: &ActorId, token_id: U256);
+    fn safe_batch_transfer_from(&mut self, from: &ActorId, to: &ActorId, token_id: U256);
+
+    fn approve_for_all(&mut self, owner: &ActorId, operator: &ActorId, approved: bool);
+    fn is_approved_for_all(&self, account: &ActorId, ids)
+
+    fn balance_of(&self, account: &ActorId);
+    fn balance_of_batch(&self, accounts: &ActorId[], ids)
+
+    fn owner_of(&self, token_id: U256);
+}
