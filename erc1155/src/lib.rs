facab8b5888e420dcaf4ae29c84f7e656e521d16
liushooter <shooter.hero@gmail.com>
2022-01-27 20:18:45 +0800

    mint_batch

--- erc1155/src/lib.rs
+++ erc1155/src/lib.rs
@@ -20,7 +20,7 @@ struct Erc1155Token {
     symbol: String,
     description: String,
     base_uri: String,
-    balances: BTreeMap<u128, BTreeMap<ActorId, u128>>
+    balances: BTreeMap<u128, BTreeMap<ActorId, u128>>,
 }
 
 static mut ERC1155_TOKEN: Erc1155Token = Erc1155Token {
@@ -38,8 +38,45 @@ impl Erc1155Token {
         *self.balances.get(id).unwrap().get(account).unwrap_or(&0)
     }
 
+    fn set_balance(&mut self, account: &ActorId, id: &u128, amount: u128) {
+        let mut _balance = self.balances.get(id).unwrap().clone();
+        _balance.insert(*account, amount);
+    }
+
     fn balance_of(&self, account: &ActorId, id: &u128) -> u128 {
         self.get_balance(account, id)
+    }
+
+    fn mint(&mut self, account: &ActorId, id: &u128, amount: u128) {
+        // check owner
+        if account == &ZERO_ID {
+            panic!("ERC1155: Mint to zero address");
+        }
+        unsafe {
+            let old_balance = ERC1155_TOKEN.get_balance(account, id);
+            self.set_balance(account, id, old_balance.saturating_add(amount));
+        }
+        // TransferSingle event
+    }
+
+    fn mint_batch(&mut self, account: &ActorId, ids: &[u128], amounts: &[u128]) {
+        if account == &ZERO_ID {
+            panic!("ERC1155: Mint to zero address");
+        }
+
+        if ids.len() != amounts.len() {
+            panic!("ERC1155: ids and amounts length mismatch");
+        }
+
+        for (i, ele) in ids.iter().enumerate() {
+            let amount = amounts[i];
+            unsafe {
+                let old_balance = ERC1155_TOKEN.get_balance(account, ele);
+                self.set_balance(account, ele, old_balance.saturating_add(amount));
+            }
+        }
+
+        // TransferBatch event
     }
 }
 
